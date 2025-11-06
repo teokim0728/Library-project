3d4170faac3d8b99a884c65ea70120808364b196
teokim0728 <teokim0728@gmail.com>
2025-11-06 16:35:58 +0900

    modified:   Library_checkout_system_UI.py 	modified:   project/module/is_in_borrowed_books.py 	modified:   project/module/update_borrowed_books.py

--- Library_checkout_system_UI.py
+++ Library_checkout_system_UI.py
@@ -718,5 +718,5 @@ if __name__ == "__main__":
     check_unreturned_books_thread.start()
 
     # Use host='0.0.0.0' if running in Docker/remote. Port from env if provided.
-    port = int(os.getenv("PORT", "8000"))
+    port = int(os.getenv("PORT", "80"))
     app.run(host="0.0.0.0", port=port, debug=DEBUG)
--- project/module/is_in_borrowed_books.py
+++ project/module/is_in_borrowed_books.py
@@ -1,6 +1,6 @@
 import os
 from project.module.update_log import *
-def is_in_borrowed_books(isbn,s_id):
+def is_in_borrowed_books(isbn,s_id = 0):
     borrowed = open("borrowed_books.txt", "r", encoding='UTF8')
     existing_info = False
     key = get_key()
@@ -8,8 +8,8 @@ def is_in_borrowed_books(isbn,s_id):
         line = decrypt(line,key)
         line = ''.join(line)
         line = line.split()
-        if line[2] == s_id and line[3] == isbn:
+        if line[3] == isbn:
             existing_info = True
-            update_log(8,s_id,isbn)
+            update_log(8,0,isbn)
     borrowed.close()
     return existing_info
